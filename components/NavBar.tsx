76a3428112527116553593e715e1c8084b30a49e
gavinbrown025 <gavinbrown025@gmail.com>
2025-08-04 16:02:11 +1000

    Created Auth

---
+++ components/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { checkUser } from '@/lib/checkUser'
+
+export default function NavBar() {
+  const user = checkUser();
+  return (
+    <div>NavBar</div>
+  )
+}
