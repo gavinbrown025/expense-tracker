6dbd73050cacb29c7a59d46ef1dc8afdf99191ba
gavinbrown025 <gavinbrown025@gmail.com>
2025-09-08 10:55:12 +1000

    fix nav space

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 bg-neutral shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-14 sm:h-16">
+        <div className="flex items-center justify-between gap-6 h-14 sm:h-16">
           {/* Logo Section */}
           <div className="flex flex-grow items-center">
             <Link
